52ff3a7862656cccf1eac97d47c73bd064633d46
roma77 <roma77@meta.ua>
2019-02-13 23:03:06 +0200

    Routing 2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,21 +2,32 @@
 // src/Controller/BlogController.php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
 	/**
-     * @Route("/blog/{page}", name="blog_list", requirements={"page"="\d+"})
+     * Matches /blog exactly
+     *
+# * @Route("/blog", name="blog_list")
+# * @Route("/blog/{page<\d+>}", name="blog_list")
+	 * @Route("/blog/{page}", name="blog_list", requirements={"page"="\d+"})
      */
+	 
     public function list($page = 1)
     {
         // ...'
 		// echo $page;
+		return new Response(
+            '<html><body>Lucky number: blog_list '.$page.'</body></html>'
+        );
     }
 
     /**
+     * Matches /blog/*
+     *
      * @Route("/blog/{slug}", name="blog_show")
      */
 	
@@ -24,6 +35,9 @@ class BlogController extends AbstractController
     {
         // ...
 		//  echo $slug;
+		return new Response(
+            '<html><body>Lucky number: blog_show</body></html>'
+        );
     }
 	
 	
